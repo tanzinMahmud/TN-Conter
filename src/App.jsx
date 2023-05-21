db272a50921cd77593530d896d1bd1b92da9a14c
tanzinMahmud <tangin31@gmail.com>
2023-05-21 17:52:04 +0600

    project folder structure reorganized

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,8 @@
-import React, { useState, useEffect } from 'react'
-import Button from './components/button/button'
-import Header from './components/header/header'
+import React from 'react'
+import Home from './screens/home/home'
 
 const App = () => {
-    const [counter, setCounter] = useState(0)
-
-    const increment = () => {
-        setCounter((prevState) => prevState + 1)
-    }
-
-    const decrement = () => {
-        setCounter((prevState) => prevState - 1)
-    }
-
-    const reset = () => {
-        setCounter(0)
-    }
-
-    return (
-        <>
-            <div className='container'>
-                <Header title='Counter' />
-                <div className='d-flex justify-content-between'>
-                    <Button title='-' action={decrement} />
-                    <h1>{counter}</h1>
-                    <Button title='+' action={increment} />
-                </div>
-                <div className='text-center'>
-                    <Button title='Reset' action={reset} />
-                </div>
-            </div>
-        </>
-    )
+    return <Home />
 }
 
 export default App
---
+++ src/screens/home/home.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import Button from '../../components/button/button'
+import Header from '../../components/header/header'
+
+const home = () => {
+    const [counter, setCounter] = useState(0)
+
+    const increment = () => {
+        setCounter((prevState) => prevState + 1)
+    }
+
+    const decrement = () => {
+        setCounter((prevState) => prevState - 1)
+    }
+
+    const reset = () => {
+        setCounter(0)
+    }
+
+    return (
+        <div className='container'>
+            <Header title='Counter' />
+            <div className='d-flex justify-content-between'>
+                <Button title='-' action={decrement} />
+                <h1>{counter}</h1>
+                <Button title='+' action={increment} />
+            </div>
+            <div className='text-center'>
+                <Button title='Reset' action={reset} />
+            </div>
+        </div>
+    )
+}
+
+export default home
